55539f14bfd4543d747625b3aa57f6635f3ef2a3
mlaizure <maddi.laizure@gmail.com>
2021-02-24 01:10:47 +0000

    Task 8

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+from =  ARGV[0].scan(/(?<=from:)(.*?)\]/).join
+to =  ARGV[0].scan(/(?<=to:)(.*?)\]/).join
+flags = ARGV[0].scan(/(?<=flags:)(.*?)\]/).join
+puts "#{from},#{to},#{flags}"
