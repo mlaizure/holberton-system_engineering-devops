5a1362d6dd4b501240366328ea29c3d19cd91019
mlaizure <maddi.laizure@gmail.com>
2021-04-06 18:52:26 +0000

    Task 7

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,17 @@
+# installs and configures nginx
+include stdlib
+
+package { 'nginx':
+  require => Exec['apt-get update'],
+}
+
+file_line { 'Holberton School':
+  path => '/var/www/html/index.html',
+  line => 'Holberton School',
+}
+
+file_line {'Redirect Me':
+  path  => '/etc/nginx/sites-available/default',
+  after => Pattern[/^\s+server_name .+;/],
+  line  => "\tlocation /redirect_me {\n\t\treturn 301 https://bit.ly/3rTuCnC;\n\t}",
+}
