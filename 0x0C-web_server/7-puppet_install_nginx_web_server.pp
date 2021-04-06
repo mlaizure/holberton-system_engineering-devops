2843ca1e1f7494877b91159aa2026106d6532dcc
mlaizure <maddi.laizure@gmail.com>
2021-04-06 19:47:44 +0000

    Fixed regex

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -7,7 +7,7 @@ exec { 'apt-get-update':
 
 package { 'nginx':
   ensure  => 'installed',
-  require => Exec['apt-get update'],
+  require => Exec['apt-get-update'],
 }
 
 file_line { 'Holberton School':
@@ -17,6 +17,6 @@ file_line { 'Holberton School':
 
 file_line {'Redirect Me':
   path  => '/etc/nginx/sites-available/default',
-  after => Pattern[/^\s+server_name .+;/],
+  after => '^\s+server_name .+;',
   line  => "\tlocation /redirect_me {\n\t\treturn 301 https://bit.ly/3rTuCnC;\n\t}",
 }
