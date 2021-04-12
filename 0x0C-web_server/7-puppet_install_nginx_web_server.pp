eff74dac3ec84aa86da8480e3927559b75283188
mlaizure <maddi.laizure@gmail.com>
2021-04-12 23:08:11 +0000

    Adding ensure to file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -24,6 +24,7 @@ file_line { 'Redirect Me':
 }
 
 file { 'Error Page Text':
+  ensure  => 'file'
   path    => '/var/www/html/my_404.html',
   content => "Ceci n'est pas une page",
   require => Package['nginx'],
