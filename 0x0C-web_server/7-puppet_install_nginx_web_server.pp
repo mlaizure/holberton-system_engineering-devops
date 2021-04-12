0fa425eb184ccc372de6b04f67e7c4dff56debe1
mlaizure <maddi.laizure@gmail.com>
2021-04-12 23:02:45 +0000

    Changing dependency tree

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,39 +5,33 @@ exec { 'apt-get-update':
   command => '/usr/bin/env apt-get -y update',
 }
 
-package { 'nginx':
-  ensure  => 'installed',
-  require => Exec['apt-get-update'],
+-> package { 'nginx':
+  ensure => 'installed',
 }
 
-file_line { 'Holberton School':
-  path    => '/var/www/html/index.html',
-  line    => 'Holberton School',
-  require => Package['nginx'],
+-> file_line { 'Holberton School':
+  path => '/var/www/html/index.html',
+  line => 'Holberton School',
 }
 
-file_line { 'Redirect Me':
-  path    => '/etc/nginx/sites-available/default',
-  after   => '^\s+server_name .+;',
-  line    => "\tlocation /redirect_me {\n\t\treturn 301 https://bit.ly/3rTuCnC;\n\t}",
-  require => Package['nginx'],
+-> file_line { 'Redirect Me':
+  path  => '/etc/nginx/sites-available/default',
+  after => '^\s+server_name .+;',
+  line  => "\tlocation /redirect_me {\n\t\treturn 301 https://bit.ly/3rTuCnC;\n\t}",
 }
 
-file { 'Error Page Text':
+-> file { 'Error Page Text':
   path    => '/var/www/html/my_404.html',
   content => "Ceci n'est pas une page",
-  require => Package['nginx'],
 }
 
-file_line { 'Error Page Config':
-  path    => '/etc/nginx/sites-available/default',
-  after   => '^\s+server_name .+;',
-  line    => "\terror_page 404 /my_404.html;\n\tlocation /my_404.html {\n\t\troot /var/www/html;\n\t\tinternal;\n\t}",
-  require => Package['nginx'],
+-> file_line { 'Error Page Config':
+  path  => '/etc/nginx/sites-available/default',
+  after => '^\s+server_name .+;',
+  line  => "\terror_page 404 /my_404.html;\n\tlocation /my_404.html {\n\t\troot /var/www/html;\n\t\tinternal;\n\t}",
 }
 
-service { 'nginx':
+-> service { 'nginx':
   ensure  => 'running',
   restart => 'service nginx restart',
-  require => Package['nginx'],
 }
