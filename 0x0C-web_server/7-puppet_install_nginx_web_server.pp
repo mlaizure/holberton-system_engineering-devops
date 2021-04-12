255cee93507183fa00f4d92f92add166a15a94ad
mlaizure <maddi.laizure@gmail.com>
2021-04-12 22:47:34 +0000

    Reverting again with no changes

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,22 +5,26 @@ exec { 'apt-get-update':
   command => '/usr/bin/env apt-get -y update',
 }
 
--> package { 'nginx':
-  ensure => 'installed',
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['apt-get-update'],
 }
 
--> file_line { 'Holberton School':
-  path => '/var/www/html/index.html',
-  line => 'Holberton School',
+file_line { 'Holberton School':
+  path    => '/var/www/html/index.html',
+  line    => 'Holberton School',
+  require => Package['nginx'],
 }
 
--> file_line {'Redirect Me':
-  path  => '/etc/nginx/sites-available/default',
-  after => '^\s+server_name .+;',
-  line  => "\tlocation /redirect_me {\n\t\treturn 301 https://bit.ly/3rTuCnC;\n\t}",
+file_line {'Redirect Me':
+  path    => '/etc/nginx/sites-available/default',
+  after   => '^\s+server_name .+;',
+  line    => "\tlocation /redirect_me {\n\t\treturn 301 https://bit.ly/3rTuCnC;\n\t}",
+  require => Package['nginx'],
 }
 
--> service {'nginx':
+service {'nginx':
   ensure  => 'running',
   restart => 'service nginx restart',
+  require => Package['nginx'],
 }
