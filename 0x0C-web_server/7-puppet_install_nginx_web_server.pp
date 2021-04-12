d436c7973fce5b45b59e0b396883e6c881d1c0a2
mlaizure <maddi.laizure@gmail.com>
2021-04-12 21:50:59 +0000

    Adding requirements

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -11,17 +11,20 @@ package { 'nginx':
 }
 
 file_line { 'Holberton School':
-  path => '/var/www/html/index.html',
-  line => 'Holberton School',
+  path    => '/var/www/html/index.html',
+  line    => 'Holberton School',
+  require => Package['nginx'],
 }
 
 file_line {'Redirect Me':
-  path  => '/etc/nginx/sites-available/default',
-  after => '^\s+server_name .+;',
-  line  => "\tlocation /redirect_me {\n\t\treturn 301 https://bit.ly/3rTuCnC;\n\t}",
+  path    => '/etc/nginx/sites-available/default',
+  after   => '^\s+server_name .+;',
+  line    => "\tlocation /redirect_me {\n\t\treturn 301 https://bit.ly/3rTuCnC;\n\t}",
+  require => Package['nginx'],
 }
 
 service {'nginx':
   ensure  => 'running',
   restart => 'service nginx restart',
+  require => Package['nginx'],
 }
