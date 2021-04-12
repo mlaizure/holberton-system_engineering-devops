520d82e1fca617f39744c40f0796df90622b5a78
mlaizure <maddi.laizure@gmail.com>
2021-04-12 22:59:44 +0000

    Adding commas

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -16,14 +16,27 @@ file_line { 'Holberton School':
   require => Package['nginx'],
 }
 
-file_line {'Redirect Me':
+file_line { 'Redirect Me':
   path    => '/etc/nginx/sites-available/default',
   after   => '^\s+server_name .+;',
   line    => "\tlocation /redirect_me {\n\t\treturn 301 https://bit.ly/3rTuCnC;\n\t}",
   require => Package['nginx'],
 }
 
-service {'nginx':
+file { 'Error Page Text':
+  path    => '/var/www/html/my_404.html',
+  content => "Ceci n'est pas une page",
+  require => Package['nginx'],
+}
+
+file_line { 'Error Page Config':
+  path    => '/etc/nginx/sites-available/default',
+  after   => '^\s+server_name .+;',
+  line    => "\terror_page 404 /my_404.html;\n\tlocation /my_404.html {\n\t\troot /var/www/html;\n\t\tinternal;\n\t}",
+  require => Package['nginx'],
+}
+
+service { 'nginx':
   ensure  => 'running',
   restart => 'service nginx restart',
   require => Package['nginx'],
