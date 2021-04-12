30219e88c4fed80993430ce007b349e86aa42a67
mlaizure <maddi.laizure@gmail.com>
2021-04-12 23:27:45 +0000

    Added nginx install

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,24 @@
 # creating custom HTTP header
 include stdlib
 
-file_line { 'Redirect Me':
-  path  => '/etc/nginx/sites-available/default',
-  after => '^\s+server_name .+;',
-  line  => "\n\tadd_header X-Served-By ${hostname};",
+exec { 'apt-get-update':
+  command => '/usr/bin/env apt-get -y update',
+}
+
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['apt-get-update'],
+}
+
+file_line { 'X-Served-By':
+  path    => '/etc/nginx/sites-available/default',
+  after   => '^\s+server_name .+;',
+  line    => "\n\tadd_header X-Served-By ${hostname};",
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure  => 'running',
+  restart => 'service nginx restart',
+  require => Package['nginx'],
 }
