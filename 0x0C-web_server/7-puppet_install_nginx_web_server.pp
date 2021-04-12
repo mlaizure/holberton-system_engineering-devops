ce477fb10cbbf14178072c7debd03fff777366dc
mlaizure <maddi.laizure@gmail.com>
2021-04-12 21:37:11 +0000

    Starting nginx service

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,7 +2,7 @@
 include stdlib
 
 exec { 'apt-get-update':
-  command => '/usr/bin/apt-get update',
+  command => '/usr/bin/env apt-get -y update',
 }
 
 package { 'nginx':
@@ -19,4 +19,9 @@ file_line {'Redirect Me':
   path  => '/etc/nginx/sites-available/default',
   after => '^\s+server_name .+;',
   line  => "\tlocation /redirect_me {\n\t\treturn 301 https://bit.ly/3rTuCnC;\n\t}",
+}
+
+service {'nginx':
+  ensure  => 'running',
+  restart => 'service nginx restart',
 }
