de9b210b1bd57a6bb315293ddf58dfc0a270b312
mlaizure <maddi.laizure@gmail.com>
2021-05-18 18:58:22 +0000

    Task 2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""queries Reddit API"""
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """prints titles of first 10 hot posts for a given subreddit"""
+    import requests
+
+    if not after:
+        url = 'https://api.reddit.com/r/{}/hot'.format(subreddit)
+    else:
+        url = 'https://api.reddit.com/r/{}/hot?after={}'.format(
+            subreddit, after)
+
+    sub_info = requests.get(url,
+                            headers={"user-agent": "user"},
+                            allow_redirects=False).json()
+
+    if 'data' not in sub_info:
+        return None
+
+    for post in sub_info['data']['children']:
+        hot_list.append(post['data']['title'])
+    after = sub_info['data']['after']
+
+    if after:
+        return recurse(subreddit, hot_list, after)
+    return hot_list
